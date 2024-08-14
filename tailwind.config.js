a9882765456530eeee3d273b5dccf11ce4927393
Shivam Vishwakarma <shivamvishva552@gmail.com>
2024-08-15 00:08:10 +0530

    resposive

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,39 @@ export default {
         },
       },
       backgroundImage: {
-        mainGradient:"linear-gradient(to right, #27A4FF, #3354FF 28%, #7433FF 50%, #F64ABB 79%, #FF7C33)",
+        mainGradient:
+          "linear-gradient(to right, #27A4FF, #3354FF 28%, #7433FF 50%, #F64ABB 79%, #FF7C33)",
       },
+      keyframes: {
+        inSlideUp: {
+          "0%": { transform: "translateY(50%) scale(0.8)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        inLeft: {
+          "0%": { transform: "translateX(-50%) scale(0.8)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        inSlideDown: {
+          "0%": { transform: "translateY(-50%) scale(0.8)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        inRight: {
+          "0%": { transform: "translateX(50%) scale(0.8)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        scrollSpin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+    },
+    animation: {
+      inSlideUp: "inSlideUp 0.5s cubic-bezier(.49,-0.34,.49,1.58)",
+      inLeft: "inLeft 0.5s cubic-bezier(.49,-0.34,.49,1.58)",
+      inSlideDown: "inSlideDown 0.5s cubic-bezier(.49,-0.34,.49,1.58)",
+      inRight: "inRight 0.5s cubic-bezier(.49,-0.34,.49,1.58)",
+      scrollSpin: "scrollSpin 1s view infinite",
     },
   },
   plugins: [],
 };
-
